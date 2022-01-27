6f80e94f4da0d4df6b203dfe2f170992a5137e47
Adwaith66 <adwaith66@gmail.com>
2022-01-27 15:02:26 -0600

    Attempt at neo4j

--- CalculatorApi/src/main/java/com/calculator/springapi/controller/CalculatorController.java
+++ CalculatorApi/src/main/java/com/calculator/springapi/controller/CalculatorController.java
@@ -1,5 +1,11 @@
 package com.calculator.springapi.controller;
 
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.calculator.springapi.model.CalculatedResult;
 import com.calculator.springapi.model.ResultParser;
 
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping(
@@ -17,9 +24,22 @@ import com.calculator.springapi.model.ResultParser;
 public class CalculatorController {
 	
 	private CalculatedResult cr;
+	String uri = "neo4j://localhost:7687";
+	
+	public enum NodeType implements Label{
+		
+	}
+
 
 	@GetMapping("/calculate/{values}")
 	public String getMapping(@PathVariable("values") String values) throws Exception {
+		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
+		GraphDatabaseService graphDB = dbFactory.newEmbeddedDatabase("/Users/adwaithsreekumar/Desktop/CalculatorAPIReact/SpringReactAPICalculator/CalculatorApi/neo4j-community-2.1.6/data/graph.db");
+		
+		
+		
+		
+		
 		try(ResultParser rs = new ResultParser(values)){
 			cr = rs.parseQuery();
 			return cr.toString();
